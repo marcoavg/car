8ed1b468d3bafe103a3349af83bd2644ceb40682
marcoavg <marcoavg1@gmail.com>
2019-01-22 17:35:10 -0600

    add items

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs')
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3002
+
+app.use(express.static(__dirname + '/public'))
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+
+app.listen(port, () => {
+    console.log('escuchando peticiones ' + port);
+})
